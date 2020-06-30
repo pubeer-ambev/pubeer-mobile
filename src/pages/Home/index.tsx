915c8c6540ed71303637eded2d7f19a3fc94f660
Bianca Veronezi <bds.veronezi@gmail.com>
2020-06-29 22:57:20 -0300

    primeiro commit

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, Image, Text, StyleSheet } from "react-native";
+
+const Home = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Image source={require("../../assets/logo.png")} />
+        <Text style={styles.title}>Recriando o conceito de bar</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 32,
+    backgroundColor: "#272C33",
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  title: {
+      color: "#F2A951",
+      fontSize: 25,
+  }
+});
+
+export default Home;
