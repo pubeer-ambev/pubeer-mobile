8edf0d1b1226f999c1c3129e956d289b6dffafc0
Bianca Veronezi <bds.veronezi@gmail.com>
2020-06-30 22:10:06 -0300

    icon png

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { View, Text, TextInput, StyleSheet, Image, Button } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
 
-
+import { Svg, Circle, Rect } from "react-native-svg";
 
 const Login = () => {
   const navigation = useNavigation();
@@ -15,36 +15,41 @@ const Login = () => {
   function navegarParaCadastrar() {
     navigation.navigate("Cadastrar");
   }
-  
+
   return (
     <View style={styles.container}>
-      <View style={styles.main}>
-      <Image style={styles.logo} source={require("../../assets/logo-login.png")} />
-        <RectButton style={[styles.entrar]}>
-             <Text style={styles.facebook}>
-             <Image source={require("../../assets/face-icon.svg")} /> 
-             Entrar com Facebook
-            </Text>
-        </RectButton>
-          <RectButton  style={[styles.entrar]}>
-             <Text style={styles.google}>
-             <Image source={require("../../assets/google-icon.svg")} /> 
-             Entrar com Google
-            </Text>
-        </RectButton>
-        <RectButton style={[styles.entrar]} onPress={navegarParaEntrar}>
-             <Text style={styles.email}>
-             <Image source={require("../../assets/email-icon.svg")} /> 
-             Entrar com E-mail
-            </Text>
-        </RectButton>
-        <Text style={styles.conta}>
-          Não tem uma conta? 
-          <Text style={styles.cadastre} onPress={navegarParaCadastrar}> Cadastre-se</Text>
+      <View style={styles.logo}>
+        <Image source={require("../../assets/logo-login.png")} />
+        <View>
+          <Text style={styles.slogan}>Recriando o seu conceito de bar</Text>
+        </View>
+      </View>
+      <RectButton style={[styles.entrar]}>
+        <Text style={styles.facebook}>
+          <Image
+            style={{ marginRight: 50 }}
+            source={require("../../assets/face.png")}
+          />
+          Entrar com Facebook
         </Text>
-        <Text style={styles.ajuda}>Precisando de ajuda?</Text>
-        <Text style={styles.faleConosco}>Fale conosco</Text>
-      </View> 
+      </RectButton>
+      <RectButton style={[styles.entrar]}>
+        <Text style={styles.google}>
+          <Image source={require("../../assets/google-icon.png")} />
+          Entrar com Google
+        </Text>
+      </RectButton>
+      <RectButton style={[styles.entrar]}>
+        <Text style={styles.email}>
+          <Image source={require("../../assets/email-icon.png")} />
+          Entrar com E-mail
+        </Text>
+      </RectButton>
+      <Text style={styles.conta}>
+        Não tem uma conta? <Text style={styles.cadastre}> Cadastre-se</Text>
+      </Text>
+      <Text style={styles.ajuda}>Precisando de ajuda?</Text>
+      <Text style={styles.faleConosco}>Fale conosco</Text>
     </View>
   );
 };
@@ -52,80 +57,87 @@ const Login = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 24,
-    backgroundColor: '#000'
-  },
-  main: {
-    flex: 1
+    padding: 20,
+    justifyContent: "center",
+    backgroundColor: "#000000",
   },
   logo: {
-    marginBottom: 32,
-    left: 50
+    padding: 14,
+  },
+  slogan: {
+    fontSize: 20,
+    color: "#F2A951",
+    textAlign: "center",
   },
   entrar: {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: '500',
-    margin: 18
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "500",
+    margin: 5,
   },
   facebook: {
-    backgroundColor: '#3D5A99',
-    padding: 18,
-    color: '#FFFFFF',
-    textAlign: 'center',
+    backgroundColor: "#3D5A99",
+    height: 50,
+    maxHeight: 50,
+    color: "#FFFFFF",
+    textAlign: "center",
+    padding: 10,
     fontSize: 20,
   },
-  google:{
-    backgroundColor: '#FFFFFF',
-    padding: 18,
-    color: '#757575',
-    textAlign: 'center',
+  google: {
+    backgroundColor: "#FFFFFF",
+    height: 50,
+    maxHeight: 50,
+    padding: 10,
+    color: "#757575",
+    textAlign: "center",
     fontSize: 20,
   },
-  email:{
-    backgroundColor: '#FFFFFF',
-    padding: 18,
-    color: '#757575',
-    textAlign: 'center',
+  email: {
+    backgroundColor: "#FFFFFF",
+    height: 50,
+    maxHeight: 50,
+    color: "#757575",
+    textAlign: "center",
     fontSize: 20,
   },
   conta: {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "normal",
     fontSize: 18,
-    color: '#F2A951',
-    textAlign: 'center',
+    color: "#F2A951",
+    textAlign: "center",
     paddingTop: 46,
   },
-  cadastre:{
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
+  cadastre: {
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "normal",
     fontSize: 18,
-    color: '#F2A951',
-    textAlign: 'center',
+    color: "#F2A951",
+    textAlign: "center",
     paddingTop: 46,
-    textDecorationLine: 'underline'
+    textDecorationLine: "underline",
   },
   ajuda: {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "normal",
     fontSize: 18,
-    color: '#F2A951',
-    textAlign: 'center',
+    color: "#F2A951",
+    textAlign: "center",
     paddingTop: 40,
   },
   faleConosco: {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "normal",
     fontSize: 18,
-    color: '#F2A951',
-    textAlign: 'center',
-    textDecorationLine: 'underline'
-  }
+    color: "#F2A951",
+    textAlign: "center",
+    textDecorationLine: "underline",
+  },
 });
 
 export default Login;
