9555e1131e0ef778ede9118eca5c1add8e2eaa18
Bianca Veronezi <bds.veronezi@gmail.com>
2020-07-03 19:55:05 -0300

    iniciando dialoglow

---
+++ config.tsx
@@ -0,0 +1,14 @@
+export const dialogflowConfig = {
+  type: "service_account",
+  project_id: "pubeerbot-eihcbw",
+  private_key_id: "09366659561bd17847dc168035d597c67698386b",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDSKGEMXmpk4KWV\nY6ecRIscaO4w62mTEoXAX2YoFh4Aub3Q8VhA90Gah4rEjBn9dc/zsLGO9U4ngyOy\nmCczg+Rbp3/cSWjXliDCEvE71DLIIRoErwgc5T+NMJApUoJKnnCqCr4mPL45dS3o\nLMh9RKoMD1NW93RT3RhM0Rk1TQdIqf5vtCPHhZlVnekgMUOVo2MHBoMKSjjWVU9K\n6zrALTmU+x3p8mpNTA84CWsbB6vcBCI3jLAanIIKEYYnRKy8kDD6bSg398m7hzzx\nqJAgPs2/J2+Xg3ikkqV1OFcobvZtwgL5imu4Je6/KB6oRHORIOQv4ljJclWhCCQz\nlgHZiJnFAgMBAAECggEAA6Fk8YJ4m8OEAejMqgSP98oZ+P86YmHj9ebv/mokikHP\nvlomHyXBwV7tMp8pihAV9Egtj+tgEdQZaaDWZtAinQxvkE3s6nI3Dl6m1HlUrGQZ\nbWS29U6LYqaWi4cOpgFZQcIrwC41Twvv4qebZVRPDEjbfdmKgDIYS8yQWn05Kf0o\nEYvzxSsBxafT+SS9BjuY6/+R683cPpRDYOOihYufntB3paq58BxkZLRtA2I0NHO/\n4GzaNC4OCpeVvZbK1LMSt/heXh4lOF+ZnWfSaMD5svm34M16G3jwMyrQe4wsJPeN\nx1P0+/lxcJxa/5SdyDyAt7BnMm/hivHch/i3lqcCJQKBgQDrnDJDExf2Tf+jiYiz\ncl4BNYVSUx/e4wOL6RZWZ8fbsu4Kc/yqiN7bwhOQs5hgPoOZkVrcgixWS1anRC4O\nudaHvUXmz3SkgXrEBHZ5Gb35330x2x6TzRRsGI8cnWI0D+NNI7gv22029/1+dqdi\n5joHiSPOTYpLhvCL8uKPLYJRuwKBgQDkWEyzQoOmC66SYPjD7P2qyPZe9AJAp6X0\nOM3SWBrOfRW5ykAijcrctKszT+8MmwZrV+OkAY7Gt4vcAUzbDHhY3BBGwehOXDN/\nMSizDBG7j452JelrpyhfZA4gAVsyEhdUgE+QF1n6FWK6LgSklVLcWXseTj6uJJBV\nAPGRoaZKfwKBgB42hWt8g1IeZm2+MT1xqQRWrKYF6sxWKBpE5Yxm+oX9x/CnNvtX\nWdysYn3nyd5tpcqHjTNBrhXSy2XfpFhXFF+Lse8LI1YFFI0WVWrqFiZ4tkEybBVf\n8wJ5k8CDnhg3AVre/buFU9f8rREOfiB8jWxx9Zl68AUHZMlNeCgMGe4JAoGAYRLc\nwJR1TSDIPTh2aSyWBoXanzFSx9O9rF59uX91HfV1R/+gVlA7Pqqb4XuI364EA5xg\njTeQUBB1p7jxyGm7u+MKFFqY/L8bsWXfrIZj/32fDcE+Q2LTugHaM2cAMwAX5PeL\nW3Fh8fwndYjctm84w65SteaDpRqsNUbqa4PyItcCgYB0FiYmhtSYDI9o7zYtuUv8\nv8Ba0UP9Y7DEUge/ZT91WPxu3y627D+FOZzZqptTWEQbykW3pF1wDxmeSABESNPz\nUEPfnpWW4AG/1jwkpXyS72/8lrEiy8a275cC1EBsuHVWILswZGmJ5cKVboZc9hsI\ntcsjh14RJ3P0KQtUPX572A==\n-----END PRIVATE KEY-----\n",
+  client_email: "dialogflow-ipycne@pubeerbot-eihcbw.iam.gserviceaccount.com",
+  client_id: "113067675877865616588",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/dialogflow-ipycne%40pubeerbot-eihcbw.iam.gserviceaccount.com",
+};
--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,108 +1,182 @@
-import React from "react"
-import { View, SafeAreaView, Platform, StyleSheet, TouchableOpacity, Text, Image } from "react-native"
-import { AntDesign } from '@expo/vector-icons';
-import { useNavigation, useRoute } from "@react-navigation/native"
-import { TextInput } from "react-native-gesture-handler";
+import React, { useState, useEffect, useCallback } from "react";
+import {
+  View,
+  SafeAreaView,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Image,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { GiftedChat, Send } from "react-native-gifted-chat";
+import { Dialogflow_V2 } from "react-native-dialogflow";
+import { dialogflowConfig } from "../../../config";
+
+const BOT_USER = {
+  _id: 2,
+  name: "PubeerBot",
+};
 
 interface Params {
-    chatName: string;
+  chatName: string;
 }
 
 const Chat = () => {
+  const [messages, setMessages] = useState([
+    {
+      _id: 1,
+      text: `Fala galera`,
+      createdAt: new Date(),
+      user: BOT_USER,
+    },
+  ]);
 
-    const [value, onChangeText] = React.useState('');
+  const [spineer, setSpineer] = useState(false);
 
-    const navigation = useNavigation();
+  useEffect(() => {
+    Dialogflow_V2.setConfiguration(
+      dialogflowConfig.client_email,
+      dialogflowConfig.private_key,
+      Dialogflow_V2.LANG_PORTUGUESE_BRAZIL,
+      dialogflowConfig.project_id
+    );
+  }, []);
 
-    const route = useRoute();
+  const navigation = useNavigation();
 
-    const routeParams = route.params as Params;
+  const route = useRoute();
 
-    function handleNavigateBack() {
-        navigation.goBack();
-    }
+  const routeParams = route.params as Params;
 
-    return (
-        <SafeAreaView style={styles.droidSafeArea}>
-            <View style={styles.container}>
-                <View style={styles.header}>
-                    <View style={{ marginLeft: 10, width: '10%' }}>
-                        <TouchableOpacity onPress={handleNavigateBack}>
-                            <AntDesign name="left" size={40} color="#FFFF" />
-                        </TouchableOpacity>
-                    </View>
+  function handleNavigateBack() {
+    navigation.goBack();
+  }
 
-                    <View style={{ width: '80%' }}>
-                        <Text style={styles.title}>{routeParams.chatName}</Text>
-                    </View>
-                </View>
-            </View>
+  function handleGoogleResponse(result: any) {
+    console.log(result);
+    let text = result.queryResult.fulfillmentText;
+    sendBotResponse(text);
+  }
 
-            <View style={styles.rodape}>
-                <TextInput
-                    style={styles.inputChat}
-                    onChangeText={text => onChangeText(text)}
-                    value={value}
-                />
-                <Image
-                    style={{ alignSelf: 'center' }}
-                    source={require("../../assets/send.png")}
-                />
-            </View>
-        </SafeAreaView>
-    )
-}
+  function onSend(newMessages: any) {
+    setMessages((prevMessages) => [...newMessages, ...prevMessages]);
+
+    let message = newMessages[0].text;
+
+    Dialogflow_V2.requestQuery(
+      message,
+      (result) => handleGoogleResponse(result),
+      (error) => console.log(error)
+    );
+  }
+
+  function sendBotResponse(text: any) {
+    let msg = {
+      _id: Math.random(),
+      text,
+      createdAt: new Date(),
+      user: BOT_USER,
+    };
+
+    setMessages((prevMessages) => [msg, ...prevMessages]);
+  }
+
+  return (
+    <SafeAreaView style={styles.droidSafeArea}>
+      <View style={styles.container}>
+        <View style={styles.header}>
+          <View style={{ marginLeft: 10, width: "10%" }}>
+            <TouchableOpacity onPress={handleNavigateBack}>
+              <AntDesign name="left" size={40} color="#FFFF" />
+            </TouchableOpacity>
+          </View>
+
+          <View style={{ width: "80%" }}>
+            <Text style={styles.title}>{routeParams.chatName}</Text>
+          </View>
+        </View>
+        <View style={{ flex: 1 }}>
+          <GiftedChat
+            placeholder={"Fale com a galera"}
+            messages={messages}
+            onSend={(messages) => onSend(messages)}
+            user={{
+              _id: 1,
+            }}
+          />
+          <Send label={"Enviar"} containerStyle={{ backgroundColor: "red" }} />
+        </View>
+      </View>
+
+      {/* <View style={styles.rodape}>
+        <TextInput
+          style={styles.inputChat}
+          onChangeText={(text) => onChangeText(text)}
+          value={value}
+        />
+        <Image
+          style={{ alignSelf: "center" }}
+          source={require("../../assets/send.png")}
+        />
+      </View> */}
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    droidSafeArea: {
-        flex: 1,
-        backgroundColor: '#F2A951',
-        paddingTop: Platform.OS === 'android' ? 25 : 0
-    },
-    container: {
-        flex: 1,
-        backgroundColor: "#F6EDE3",
-    },
-    header: {
-        backgroundColor: "#F2A951",
-        width: "100%",
-        height: 80,
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    title: {
-        color: "#FFF",
-        fontSize: 34,
-        fontWeight: "bold",
-        textAlign: 'center'
-    },
-    containerChat: {
-        backgroundColor: '#F6EDE3',
-        width: "100%",
-    },
-    chat: {
-        height: '60%',
-        borderColor: '#B5A593',
-        borderWidth: 2,
-    },
-    rodape: {
-        backgroundColor: '#F6EDE3',
-        width: "100%",
-        padding: 10,
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-    },
-    inputChat: {
-        borderRadius: 20,
-        padding: 10,
-        height: 55,
-        width: '80%',
-        borderColor: '#B5A593',
-        borderWidth: 2,
-        fontSize: 16
-    }
+  droidSafeArea: {
+    flex: 1,
+    backgroundColor: "#F2A951",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
+  },
+  spinnerTextStyle: {
+    color: '#FFF'
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#F6EDE3",
+  },
+  header: {
+    backgroundColor: "#F2A951",
+    width: "100%",
+    height: 80,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  title: {
+    color: "#FFF",
+    fontSize: 34,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  containerChat: {
+    backgroundColor: "#F6EDE3",
+    width: "100%",
+  },
+  chat: {
+    height: "60%",
+    borderColor: "#B5A593",
+    borderWidth: 2,
+  },
+  rodape: {
+    backgroundColor: "#F6EDE3",
+    width: "100%",
+    padding: 10,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+  inputChat: {
+    borderRadius: 20,
+    padding: 10,
+    height: 55,
+    width: "80%",
+    borderColor: "#B5A593",
+    borderWidth: 2,
+    fontSize: 16,
+  },
 });
-
 
 export default Chat;
